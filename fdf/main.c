7e5a40f40558ddfa81ff82d6e2f4dfbf00a1a406
Ismail Najah <inajah@e1r8p6.1337.ma>
2024-11-16 12:01:14 +0100

    added ft_draw_rectangle + gradien colors

--- fdf/main.c
+++ fdf/main.c
@@ -6,7 +6,7 @@
 /*   By: inajah <inajah@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 14:26:50 by inajah            #+#    #+#             */
-/*   Updated: 2024/11/16 10:20:24 by inajah           ###   ########.fr       */
+/*   Updated: 2024/11/16 11:58:53 by inajah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,10 @@
 #define SIDE_W (WIN_W - MAIN_W)
 #define SIDE_H (WIN_H / 2)
 
+#define C_WHITE 0xFFFFFFFF
+#define C_RED	0x55FF0000
+#define C_GREEN 0x5500FF00
+#define C_BLUE  0x550000FF
 
 #define KEY_ESC 53 
 
@@ -54,6 +58,8 @@ typedef struct s_image
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
+	int		w;
+	int		h;
 }	t_image;
 
 
@@ -71,29 +77,17 @@ typedef struct s_vars
 	t_layout *layout;
 }	t_vars;
 
-void my_mlx_pixel_put(t_image *data, int x, int y, int color)
+
+int ft_fill_image(t_image *img, int w, int h, size_t color);
+
+void my_mlx_pixel_put(t_image *img, int x, int y, int color)
 {
 	char *dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	if (x < 0 || y < 0 || x >= img->w || y >= img->h)
+		return ;
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
 	*(unsigned int*) dst = color;
-}
-
-int ft_fill_image(t_image *img, int w, int h, size_t color)
-{
-	int x = 0;
-	int y = 0;
-	while (y < h)
-	{
-		x = 0;
-		while (x < w)
-		{
-			my_mlx_pixel_put(img, x, y, color);
-			x++;
-		}
-		y++;
-	}
-	return (0);
 }
 
 void	*ft_free_image(void *mlx, t_image *img)
@@ -118,6 +112,8 @@ t_image	*ft_init_image(void *mlx, int width, int height)
 	img->addr = mlx_get_data_addr(img->data, &img->bits_per_pixel, &img->line_length, &img->endian);
 	if (!img->addr)
 		return (ft_free_image(mlx, img));
+	img->w = width;
+	img->h = height;
 	return (img);
 }
 
@@ -146,11 +142,11 @@ t_layout *init_layout(void* mlx)
 	layout->top = ft_init_image(mlx, TOP_W, TOP_H);
 	if (!layout->top)
 		return (ft_free_layout(mlx, layout));
-	ft_fill_image(layout->top, TOP_W, TOP_H, 0x5500FF00);
+	ft_fill_image(layout->top, TOP_W, TOP_H, C_BLUE);
 	layout->side = ft_init_image(mlx, SIDE_W, SIDE_H);
 	if (!layout->side)
 		return (ft_free_layout(mlx, layout));
-	ft_fill_image(layout->side, SIDE_W, SIDE_H, 0x550000FF);
+	ft_fill_image(layout->side, SIDE_W, SIDE_H, C_RED);
 	return (layout);
 }
 
@@ -174,7 +170,99 @@ int ft_on_destroy(t_vars *vars)
 	return (0);
 }
 
-//draw line 
+//draw rectangle
+typedef struct s_rectangle
+{
+	int x;
+	int y;
+	int w;
+	int h;
+}	t_rectangle;
+
+t_rectangle	ft_rectangle(int x, int y, int w, int h)
+{
+	t_rectangle rec;
+	
+	rec.x = x;
+	rec.y = y;
+	rec.w = w;
+	rec.h = h;
+	return (rec);
+}
+
+int	create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
+
+int	get_t(int trgb)
+{
+	return ((trgb >> 24) & 0xFF);
+}
+
+int	get_r(int trgb)
+{
+	return ((trgb >> 16) & 0xFF);
+}
+
+int	get_g(int trgb)
+{
+	return ((trgb >> 8) & 0xFF);
+}
+
+int	get_b(int trgb)
+{
+	return (trgb & 0xFF);
+}
+
+
+unsigned int ft_lerp(unsigned int a, unsigned int b, float t)
+{
+	int sg = get_g(a);
+	int eg = get_g(b);
+
+	int out = (sg + t * (eg - sg));
+	return create_trgb(0, 255, out, 255);
+}
+
+int ft_fill_image(t_image *img, int w, int h, size_t color)
+{
+	int x = 0;
+	int y = 0;
+	while (y < h)
+	{
+		x = 0;
+		while (x < w)
+		{
+			my_mlx_pixel_put(img, x, y, ft_lerp(color, C_WHITE, (float)y / h));
+			x++;
+		}
+		y++;
+	}
+	return (0);
+}
+
+void	ft_draw_rectangle(t_image *img, t_rectangle rec)
+{
+	int i;
+
+	i = 0;
+	while (i < rec.w)
+	{
+		my_mlx_pixel_put(img, rec.x + i, rec.y, C_RED);
+		if (rec.h)
+			my_mlx_pixel_put(img, rec.x + i, rec.y + rec.h, C_RED);
+		i++;
+	}
+	i = 0;
+	while (i < rec.h)
+	{
+		my_mlx_pixel_put(img, rec.x, rec.y + i, ft_lerp(0x000099cc, C_WHITE, (float)i / rec.h));
+		if (rec.x)
+			my_mlx_pixel_put(img, rec.x + rec.w, rec.y + i, ft_lerp(0x000099cc, C_WHITE, (float)i / rec.h));
+		i++;
+	}
+}
 
 int	main(void)
 {
@@ -188,6 +276,10 @@ int	main(void)
 	vars.layout = init_layout(vars.mlx);
 	if (!vars.layout)
 		return (2);
+
+	int w = 800;
+	int h = 400;
+	ft_draw_rectangle(vars.layout->main, ft_rectangle(MAIN_W/2 - w/2, MAIN_H/2 - h/2, w, h));
 	mlx_put_image_to_window(vars.mlx, vars.win, vars.layout->top->data, MAIN_W, 0);
 	mlx_put_image_to_window(vars.mlx, vars.win, vars.layout->side->data, MAIN_W, WIN_H / 2);
 	mlx_put_image_to_window(vars.mlx, vars.win, vars.layout->main->data, 0, 0);
